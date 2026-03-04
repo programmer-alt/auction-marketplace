135792437460a6ba8805b3303d49d7d34aa1a1dc
Vadim <vadim@example.com>
2026-03-04 22:55:46 +0300

    `Update package.json and backend/src/index.ts to use dist/index.js as main file and parse PORT as integer`

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT || '5000', 10);
 
 // Middleware
 app.use(cors({
